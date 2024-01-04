c428b0178cdd6a4f8a9de3e48e48b35d37c1f243
j30ngwoo <ttangle1234@naver.com>
2024-01-05 01:54:40 +0900

    :wrench:[chore]: 프로젝트 초기 설정

---
+++ src/main/java/com/whenwhen/whenwhen/WhenwhenApplication.java
@@ -0,0 +1,13 @@
+package com.whenwhen.whenwhen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhenwhenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhenwhenApplication.class, args);
+	}
+
+}
